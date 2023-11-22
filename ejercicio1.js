90bfa4f61e51c8922d0cafd4d141d5100e94079d
juanmaherrera94 <juanmah2911@gmail.com>
2023-11-22 15:09:06 +0100

    commit reto 1 tema 4

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+const cartasJuego=[];
+let aciertos=0;
+let intentos=0;
+
+const clickImagen=()=>{
+   
+    
+}
+
+const imagen1=document.getElementById("luffy");
+
+imagen1.addEventListener("oneclick",clickImagen);
